09d57782de03db045e3ef6cba9e510323c699ee9
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-19 21:43:23 -1200

    [Sunny] Add : Select the Data In EmpDB

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -13,3 +13,5 @@ insert into EmpPayRoll values('Jay',23000,'2019.APR.22')
 insert into EmpPayRoll values('govind',20000,'2022.JUN.01')
 insert into EmpPayRoll values('bhuppi',16500,'2013.MAY.15')
 insert into EmpPayRoll values('rajesh',17000,'2014.FEB.22')
+
+select * from EmpPayRoll
