18c22e4a700036b9e551c79f48900ccefa4c0baf
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-19 22:40:02 -1200

    [Sunny] Add : Update The Emp Column

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -21,3 +21,21 @@ select Salary from EmpPayRoll where Name='Jay';
 select SatrtDate from EmpPayRoll WHERE (SatrtDate BETWEEN '2019-04-22' AND GETDATE());
 
  Alter table EmpPayRoll Add Gender varchar(1);
+
+ Update EmpPayRoll set Gender='M' where Id in(1,3);
+
+ Update EmpPayRoll set Gender='F' where Id in(2,4);
+
+ select Name from EmpPayRoll where Gender= 'F';
+
+ select SUM(Salary) from EmpPayRoll;
+
+ select SUM(Salary) from EmpPayRoll where Gender='F' group by Gender;
+
+ select AVG(Salary) from EmpPayRoll where Gender='M' group by Gender;
+
+ select MIN(Salary) from EmpPayRoll;
+
+  select MAX(Salary) from EmpPayRoll;
+ 
+ select COUNT(ID) from EmpPayRoll;
