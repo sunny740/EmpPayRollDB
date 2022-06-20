fb9f8cab05b9421a66070b81849eac15985a4f88
Sunny <sunnysejwarvitm005@gmail.com>
2022-06-19 21:20:56 -1200

    [Sunny] Add : Insert The Emp Data

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -8,3 +8,8 @@ Name varchar (20),
 Salary int,
 SatrtDate Date 
 );
+
+insert into EmpPayRoll values('Jay',23000,'2019.APR.22')
+insert into EmpPayRoll values('govind',20000,'2022.JUN.01')
+insert into EmpPayRoll values('bhuppi',16500,'2013.MAY.15')
+insert into EmpPayRoll values('rajesh',17000,'2014.FEB.22')
